02bec3ba5229947299c3fc0ea1546676779771ef
James Sully <sullyj3@gmail.com>
2014-06-08 21:46:28 +1000

    refactored into a class

--- 15.py
+++ 15.py
@@ -1,45 +1,41 @@
 #https://projecteuler.net/problem=15
-#using globals isn't very elegant. maybe ask about a nicer way of doing it
+class grid_routes_solver(object):
+    '''could be improved by dynamically altering grid size on an as needed 
+    basis, rather than defining it at instantiation.'''
 
-def gen_grid(width, height):
-    #creates a 2 dimensional grid. purpose of the grid is for each coord to hold
-    #a number representing the number of valid routes to get there
-    #makes it a square with side length as the max dimension of the rectangle 
+    def __init__(self, side_length):
+        #creates a 2 dimensional square grid. purpose of the grid is for each coord to hold
+        #a number representing the number of valid routes to get there
+        #makes it a square with side length as the max dimension of the rectangle 
 
-    side_length = max((width,height))
+        #side_length specifies the maximum grid size that can be solved
 
-    grid = [[None for y_pos in range(side_length+1)] for x_pos in range(side_length+1)]
-    return grid
+        self.grid = [[None for y_pos in range(side_length+1)] for x_pos in range(side_length+1)]
 
+    def grid_recurse(self, x, y):
+        '''returns number of possible routes to a given integer coordinate, moving
+        only right and down along integer spaced gridlines
+        make sure x and y are within bounds specified by self.
+        '''
 
-def grid_routes(width, height):
-    global grid
-    grid = gen_grid(width,height)
+        #read from existing grid to prevent unnecessary repetition
+        preexisting = self.grid[x][y]
+        if preexisting:
+            return preexisting
+            
+        if 0 in (x,y):
+            #any coord along y=0 or x=0 has only one valid route - a horizontal or
+            #vertical line from the origin to that coord.
 
-    return grid_recurse(width,height)
+            self.grid[x][y] = 1
+            return 1
 
-def grid_recurse(width, height):
-    '''returns number of possible routes to a given integer coordinate, moving
-    only right and down along integer spaced gridlines'''
+        else:
+            #number valid routes for given coordinates = sum of valid routes to its two preceding coordinates (above and to the left)
 
-    #read from existing grid to prevent unnecessary repetition
-    global grid
-    preexisting = grid[width][height]
-    if preexisting:
-        return preexisting
-        
-    if 0 in (width,height):
-        #any coord along y=0 or x=0 has only one valid route - a horizontal or
-        #vertical line from the origin to that coord.
+            num_routes = self.grid_recurse(x-1,y) + self.grid_recurse(x,y-1)
+            self.grid[x][y] = num_routes
 
-        grid[width][height] = 1
-        return 1
-
-    else:
-        #number valid routes for given coordinates = sum of valid routes to its two preceding coordinates (above and to the left)
-
-        val = grid_recurse(width-1,height) + grid_recurse(width,height-1)
-        grid[width][height] = val
-        #it'll be the same for the same rectangle on its side
-        grid[height][width] = val
-        return val
+            #it'll be the same for the same rectangle on its side
+            self.grid[y][x] = num_routes
+            return num_routes
