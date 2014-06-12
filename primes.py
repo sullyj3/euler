81c6a0483b22c89f2327fced49c2bbe4fa3e1e02
James Sully <sullyj3@gmail.com>
2014-06-13 01:52:36 +1000

    add working sieve of eratosthenes

--- primes.py
+++ primes.py
@@ -1,6 +1,9 @@
-from math import sqrt
+from math import sqrt,ceil
 
-verbosity = 1
+'''ideas:
+Could I continue an already existing sieve?'''
+
+verbosity = 2
 def debug_print(text,min_verb):
     if verbosity >= min_verb:
         print(text)
@@ -24,33 +27,36 @@ def find_n_primes(n):
         debug_print("appending {0}. we now have {1} primes".format(next_prime, primes_len), 1)
     return primes
 
-def find_primes_below_n(n):
-    #sieve of eratosthenes
+def sieve(n):
 
-    assert type(n) is int and n > 0
+    '''next version, using list instead of dict'''
+    nums = [0,1]+[True for i in range(2,n)]
 
-    numlist = [(x,True) for x in range(2,n)]
-    #that True represents whether the number is prime. We start off with the assumption that it is
-    #then toggle the truth value for every composite number
-
-    ind_current_prime = 0
-    current_prime = numlist[ind_current_prime][0]
-
-    while current_prime < n:
-        debug_print("marking multiples of {0}".format(current_prime), 1)
-
-        last_multiple_index = next(ind for ind,t in list(enumerate(numlist))[::-1] if t[0] % current_prime == 0 )
-        for ind,multiple in list(enumerate(numlist))[last_multiple_index::-current_prime]:
-            numlist[ind] = (numlist[ind][0],False)
-
-        ind_current_prime = next(ind for ind,t in list(enumerate(numlist))[ind_current_prime + 1:] if t[1] )
-        current_prime = numlist[ind_current_prime][0]
-
-    for t in numlist:
-        if t[1]:
-            yield t[0]
-            
+    #less calculatin
+    sqrtn = int(ceil(sqrt(n)))
     
+    #first prime
+    current_prime = 2
+
+    #while current_prime < sqrtn:
+    while True:
+        #mark all multiples of current_prime as composites, then make current_prime
+        #the next prime above itself, until there are no more primes below n.
+
+        position = current_prime**2
+        debug_print(position,3)
+        while position < n:
+            nums[position] = False
+            position += current_prime
+        primes_upto_sqrtn = (ind for ind,is_prime in list(enumerate(nums))[current_prime + 1:sqrtn + 1] if is_prime)
+
+        try:
+            current_prime = next(primes_upto_sqrtn)
+        except StopIteration:
+            break
+    
+    return [ind for ind,is_prime in list(enumerate(nums))[2:] if is_prime]
+
 def find_next_prime(primes):
     '''takes a list of primes. primes must contain a sequential list
     of all the primes between 0 and the next one you want to find
@@ -80,3 +86,11 @@ def find_next_prime(primes):
         else:
             debug_print("{0} is prime!".format(next_check), 2)
             return next_check
+
+def is_prime(n):
+    sqrtn = int(ceil(sqrt(n)))
+    
+    p = sieve(sqrtn)
+
+
+
